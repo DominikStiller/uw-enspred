6ea3683a02ccfadda96466f43261fd67ee57558d
Dominik Stiller <dstiller@uw.edu>
2023-12-01 14:53:30 -0800

    Add date to logging format

--- project/project/logger.py
+++ project/project/logger.py
@@ -2,7 +2,7 @@ import logging
 
 
 def get_logger(name: str):
-    logging.basicConfig()
+    logging.basicConfig(format="%(asctime)s %(levelname)s:%(name)s:%(message)s")
     logger = logging.getLogger(name)
     logger.setLevel("DEBUG")
     return logger
