090527b848ae01aa9ea860bd875b4b33e7de6137
Dominik Stiller <dstiller@uw.edu>
2023-12-02 20:15:56 -0800

    Improve dask handling

--- project/project/logger.py
+++ project/project/logger.py
@@ -2,7 +2,10 @@ import logging
 
 
 def get_logger(name: str):
-    logging.basicConfig(format="%(asctime)s %(levelname)s:%(name)s:%(message)s")
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)7s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
     logger = logging.getLogger(name)
     logger.setLevel("DEBUG")
     return logger
--- project/project/spaces.py
+++ project/project/spaces.py
@@ -41,7 +41,7 @@ class Detrend:
         time = self._get_time(time)
 
         self.time_mean = time.mean()
-        self.data_mean = data.mean(axis=1).compute()[:, np.newaxis]
+        self.data_mean = data.mean(axis=1).persist()[:, np.newaxis]
 
         time_demeaned: dask.array.Array = np.atleast_2d(time - self.time_mean).T
         state_demeaned: dask.array.Array = (data - self.data_mean).T
@@ -49,7 +49,7 @@ class Detrend:
         coeffs, _, _, _ = dask.array.linalg.lstsq(
             dask.array.from_array(time_demeaned), state_demeaned
         )
-        self.coeffs = coeffs.compute().squeeze()
+        self.coeffs = coeffs.persist().squeeze()
 
     def _linear_trend(self, time: dask.array.Array) -> dask.array.Array:
         time = self._get_time(time)
