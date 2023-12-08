190c9923ea18fcc63b00cb38924cd8cb7fd49f67
Dominik Stiller <dstiller@uw.edu>
2023-12-07 23:52:55 -0800

    Add script to compute annual averages

---
+++ project/project/scripts/compute_annual_averages.py
@@ -0,0 +1,38 @@
+import dask
+from dask.distributed import Client
+
+from project.io import IntakeESMLoader, save_mfdataset
+from project.logger import get_logger
+from project.util import get_data_path, average_annually
+
+logger = get_logger(__name__)
+
+if __name__ == "__main__":
+    client = Client(n_workers=dask.system.CPU_COUNT // 2, threads_per_worker=1)
+
+    data_path = get_data_path()
+
+    loader = IntakeESMLoader(
+        "past2k",
+        "MPI-ESM1-2-LR",
+        [
+            "zg500",
+            "pr",
+            "psl",
+            "rsut",
+            "rlut",
+            "tas",
+            "tos",
+            "zos",
+            "sos",
+            "ohc700",
+        ],
+    )
+    ds = loader.load_dataset()
+    # ds = loader.load_dataset(["702101-704012"]).isel(time=slice(None, 20))
+
+    logger.info("Averaging annually")
+    ds = average_annually(ds).chunk(chunks=dict(time=100))
+
+    save_mfdataset(ds, data_path / "annual_averages")
+    logger.info("Computing of annual averages completed")
